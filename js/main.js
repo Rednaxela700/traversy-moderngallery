81c54950b80dfca97c3fae538419804a9a861555
rednaxela700 <ohleg@windowslive.com>
2019-07-24 15:17:02 +0200

    added opacity on click

--- js/main.js
+++ js/main.js
@@ -4,11 +4,17 @@
 const [current, imgs] = [
     document.querySelector('#current'), document.querySelectorAll('.img-other img')
 ];
+const opacity = 0.4;
 
 imgs.forEach(img =>
     img.addEventListener('click', imgClick));
 function imgClick(e) {
+    //Reset opacity
+    imgs.forEach(img => (img.style.opacity = 1));
+
+    //Change current image to src of clicked image
     current.src = e.target.src;
 
+    //Change the opacity to opacity var
+    e.target.style.opacity = opacity;
 }
-console.log(imgs);
