b8026875074facf76a8ea412b176caaaa469601d
rednaxela700 <ohleg@windowslive.com>
2019-07-24 14:20:50 +0200

    basic functionality implemented

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 /**
  * Created by alexander on 24/07/2019.
  */
+const [current, imgs] = [
+    document.querySelector('#current'), document.querySelectorAll('.img-other img')
+];
+
+imgs.forEach(img =>
+    img.addEventListener('click', imgClick));
+function imgClick(e) {
+    current.src = e.target.src;
+
+}
+console.log(imgs);
